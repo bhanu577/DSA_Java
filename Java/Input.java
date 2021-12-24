8a422f66a04f87aa1a16567447daab1877d1a8c2
bhanu577 <dammalapatibhanu@gmail.com>
2021-12-24 21:48:59 +0530

    Sample program to input from keyboard

---
+++ Java/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String[] args) {
+        //Inputting vales from keyboard
+        Scanner input=new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}

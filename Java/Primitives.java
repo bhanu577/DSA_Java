03c0579c4dd87c7e7ffd1aaada6e56c8789ed0d5
bhanu577 <dammalapatibhanu@gmail.com>
2021-12-24 21:55:39 +0530

    Primitive Data types

---
+++ Java/Primitives.java
@@ -0,0 +1,19 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int a=10;
+        float fl=53.86f;
+        char letter='l';
+        String name="bhanu";
+        double doubleDecimal=76479485.348503;
+        long longInteger=758975;
+        boolean check=false;
+
+        System.out.println(a);
+        System.out.println(fl);
+        System.out.println(letter);
+        System.out.println(name);
+        System.out.println(doubleDecimal);
+        System.out.println(longInteger);
+        System.out.println(check);
+    }
+}

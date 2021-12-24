ab9f2c86ee19bfde359587c4a985e8cca8750c1e
bhanu577 <dammalapatibhanu@gmail.com>
2021-12-24 21:59:23 +0530

    Lopping and Conditions

---
+++ Java/LoopCon.java
@@ -0,0 +1,14 @@
+public class LoopCon {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(i);
+        }
+
+        int num=5;
+        while(num>0){
+            System.out.println(num);
+            num--;
+        }
+    }
+}

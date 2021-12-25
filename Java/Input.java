cfb1f24acf07e1424cbb432c5c9f57db466eb4cf
bhanu577 <dammalapatibhanu@gmail.com>
2021-12-25 21:06:25 +0530

    Java Program to fins max value of three numbers

--- Java/Input.java
+++ Java/Input.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
-
 public class Input{
     public static void main(String[] args) {
         //Inputting vales from keyboard
-        Scanner input=new Scanner(System.in);
-        System.out.println(input.nextLine());
+        Scanner in=new Scanner(System.in);
+        System.out.println(in.nextLine());
     }
 }

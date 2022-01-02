7fa503b2c50c0abc8af2fb4f606dc12e50ca8c4d
bhanu577 <dammalapatibhanu@gmail.com>
2022-01-02 19:57:21 +0530

    Binary Search modification

--- Java/Searching/BinarySearch.java
+++ Java/Searching/BinarySearch.java
@@ -14,7 +14,7 @@ public class BinarySearch {
     }
     static int search(int[] arr,int target)
     {
-        int start=0,end=arr.length;
+        int start=0,end=arr.length-1;
 
         while(start<=end){
             int mid=(start+end)/2;

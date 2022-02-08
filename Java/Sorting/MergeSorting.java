ddbdb345efca09c5897bbbaa3017875113be6bbd
bhanu577 <dammalapatibhanu@gmail.com>
2022-02-08 21:49:12 +0530

    Merge Sort Algorithm

--- Java/Sorting/MergeSorting.java
+++ Java/Sorting/MergeSorting.java
@@ -1,14 +1,16 @@
 import java.util.Arrays;
 
 class MergeSorting{
+
+    //Conqure or merge function
+
     static void merge(int[] arr,int p,int mid,int q){
-       // System.out.println(p+" "+mid+" "+q);
         int n1=mid-p+1;
         int n2=q-mid;
-
+        //creating temporary array
         int[] L=new int[n1];
         int[] R=new int[n2];
-
+        //copying original values in array arr and placing them in temporary array
         for(int i=0;i<n1;i++)
         {
             L[i]=arr[p+i];
@@ -17,6 +19,7 @@ class MergeSorting{
             R[i]=arr[mid+1+i];
         }
         int i=0,j=0,k=p;
+        // for here merge function starts
         while(i<n1 && j<n2){
             if(L[i]<=R[j]){
                 arr[k]=L[i];
@@ -36,18 +39,20 @@ class MergeSorting{
             arr[k]=R[j];
             j++;k++;
         }
-        //System.out.println(Arrays.toString(L));
-        //System.out.println(Arrays.toString(R));
-
     }
+
+    //Divide the array function
+
     static void mergeSort(int arr[],int p,int q){
         if(p<q){
-            int mid=(p+q)/2;
+            int mid=(p+q)/2; // always take floor value
             mergeSort(arr, p, mid);
             mergeSort(arr, mid+1, q);
             merge(arr,p,mid,q);
         }
     }
+
+    //Main Function
     public static void main(String[] args) {
         int[] arr={6,55,3,100,8,77,2,4};
         int first=0;
